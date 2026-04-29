94f22ec5cf88cf902af3a2936b627f74e136251e
юлини <ulini@MacBook-Air-ulini.local>
2026-04-29 12:49:21 +0500

    fix render port binding

--- server.ts
+++ server.ts
@@ -201,7 +201,7 @@ async function copySubcollectionIfMissing(sourceUid: string, targetUid: string, 
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT || 3000);
 
   app.use(express.json());
 
